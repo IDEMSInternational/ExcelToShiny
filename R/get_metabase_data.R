dbce06a261169c76abbed93ca0f4b0fa7b72173d
lilyclements <lily@idems.international>
2022-07-28 11:21:52 +0100

    get_nf_data()

--- R/get_metabase_data.R
+++ R/get_metabase_data.R
@@ -5,7 +5,7 @@
 #' @param site Connection to database to get original data (using DBI::dbConnect).
 #' @param name Data to call from connection. Default `"app_users"`, but also takes `"app_notification_interaction"`.
 #'
-#' @return Data from Metabase site
+#' @return Data from Metabase
 #' @export
 #'
 #' @examples # TODO
