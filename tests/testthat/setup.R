43ee4608a3a05644ab9a00abc90212996fc1864a
lilyclements <lily@idems.international>
2024-08-19 12:16:09 +0100

    remove testthat file

--- tests/testthat/setup.R
+++
@@ -1,6 +0,0 @@
-plh_con <- DBI::dbConnect(RPostgres::Postgres(),
-                          dbname = 'early_family_math',
-                          host = 'apps-server.idems.international',
-                          port = 5432,
-                          user = 'early_family_math',
-                          password = 'nPmtPjhi2HuQDz')
